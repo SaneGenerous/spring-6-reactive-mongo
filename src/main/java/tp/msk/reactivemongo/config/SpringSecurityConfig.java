e87ef28e748aeb30604ac3d63e23610185d6a7ad
Sane Generous <karim2k11@gmail.com>
2024-05-15 17:02:51 +0100

    Spring Security Configuration.

---
+++ src/main/java/tp/msk/reactivemongo/config/SpringSecurityConfig.java
@@ -0,0 +1,22 @@
+package tp.msk.reactivemongo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SpringSecurityConfig {
+
+    @Bean
+    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http){
+        http.csrf().disable()
+                .authorizeExchange()
+                .anyExchange().authenticated()
+                .and()
+                .oauth2ResourceServer().jwt();
+        return http.build();
+    }
+}
